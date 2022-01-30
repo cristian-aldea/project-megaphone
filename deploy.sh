91ab4eb5fc09db565ac95db4dec6afd156e870ec
Cristian Aldea <aldea.cristian@outlook.com>
2022-01-29 20:35:38 -0500

    update dependencies

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git checkout main
+git branch -D gh-pages
+git checkout -b gh-pages
+
+mkdir docs
+echo "www.cristianaldea.com" > docs/CNAME
+git add docs/
+git commit -m "Create CNAME"
+
+npm run build-prod
+mv dist/* docs
+git add docs/
+git commit -m "Deploy website"
+
+git push -u origin gh-pages --force-with-lease
+git checkout main
