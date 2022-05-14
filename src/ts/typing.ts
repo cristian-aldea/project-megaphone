eaf1209761298c8d4b1a54d7b519d7f712937d1e
Cristian Aldea <aldea.cristian@outlook.com>
2022-05-13 20:39:02 -0400

    update typing
    - typed text is no longer deleted, new text is written on new lines
    - improved/changed how cursor blinking is managed to better reflect how its done for actual cursors

--- src/ts/typing.ts
+++ src/ts/typing.ts
@@ -1,57 +1,60 @@
-import { typingCursor, typingText } from "./dom";
+import { typingText } from "./dom";
 import { sleep } from "./utils";
 
-enum TypingState {
-  TYPING,
-  DELETING,
-}
+const CHAR_SHORT_PAUSE = "@";
+const CHAR_LONG_PAUSE = "#";
+const CHAR_NEW_LINE = "$";
+const BLINKER = '<div style="display: inline-block;position: relative;top: -4px;">|</div>';
 
-const messages = [
-  "I'm a software engineer.",
-  "I love to build software...$ to solve problems!",
-  "Scroll down for more details!",
-  "Have a nice day :D",
-];
-
+const message = `I'm a software engineer.${
+  CHAR_LONG_PAUSE + CHAR_NEW_LINE + CHAR_NEW_LINE
+}I love to build software...${CHAR_SHORT_PAUSE} to solve problems!${
+  CHAR_LONG_PAUSE + CHAR_NEW_LINE + CHAR_NEW_LINE
+}Scroll down for more details!${CHAR_LONG_PAUSE + CHAR_NEW_LINE + CHAR_NEW_LINE}Have a nice day :D`;
 let text = "";
 let showCursor = true;
 
 let charIndex = 0,
-  phraseIndex = 0,
-  state = TypingState.TYPING,
   repeat = true;
 
 let editing = false;
 let stopEditTimestamp: number | null = null;
 
-const loop = false,
-  initWait = 1500,
+const initWait = 1500,
   blinkWait = 530,
   speed = 1,
   typeWait = 80 / speed,
-  typePause = 1000 / speed,
-  deleteWait = 40 / speed,
-  doneTypingWait = 2500 / speed,
-  doneDeletingWait = 1500 / speed;
+  shortWait = 500 / speed,
+  longWait = 2000 / speed;
 
 const setShowCursor = (show: boolean) => {
-  typingCursor.textContent = show ? "|" : "";
   showCursor = show;
+  updateText();
 };
 
 const setText = (value: string) => {
   text = value;
-  typingText.textContent = text;
+  updateText();
+};
+
+const updateText = () => {
+  if (showCursor) {
+    typingText.innerHTML = text + BLINKER;
+  } else {
+    typingText.innerHTML = text;
+  }
 };
 
 const animateBlinker = async () => {
   while (repeat) {
+    let wait = blinkWait;
     const elapsed = stopEditTimestamp ? performance.now() - stopEditTimestamp : null;
     if (elapsed && elapsed < blinkWait) {
-      await sleep(blinkWait - elapsed);
+      wait = blinkWait - elapsed;
       stopEditTimestamp = null;
+    } else {
+      nextBlinker();
     }
-    const wait = nextBlinker();
     await sleep(wait);
   }
   setShowCursor(false);
@@ -67,83 +70,48 @@ const animateTyping = async () => {
 
 const setEditing = (curr: boolean) => {
   editing = curr;
-  setShowCursor(true);
   if (!curr) {
     stopEditTimestamp = performance.now();
   } else {
+    setShowCursor(true);
     stopEditTimestamp = null;
   }
 };
 
-const nextBlinker = (): number => {
-  const wait = blinkWait;
+const nextBlinker = () => {
   if (editing) {
     setShowCursor(true);
   } else {
     setShowCursor(!showCursor);
   }
-
-  return wait;
 };
 
 const nextType = (): number => {
   let wait = 0;
 
-  if (state === TypingState.TYPING) {
-    const tempChar = charIndex;
-    const tempPhrase = phraseIndex;
-    const newChar = messages[tempPhrase][tempChar];
-    let pause = false;
+  const currChar = message[charIndex];
 
-    if (newChar === "$") {
-      pause = true;
-    } else {
-      setText(text + messages[tempPhrase][tempChar]);
-    }
-
-    if (charIndex === messages[phraseIndex].length - 1) {
-      // finished typing the current phrase
-      setEditing(false);
-      state = TypingState.DELETING;
-      wait = doneTypingWait;
-
-      // Handle next phrase
-      if (phraseIndex === messages.length - 1) {
-        phraseIndex = 0;
-        if (!loop) {
-          repeat = false;
-        }
-      } else {
-        phraseIndex++;
-      }
-    } else {
-      // Keep typing
-      charIndex++;
-      if (pause) {
-        setEditing(false);
-        wait = typePause;
-      } else {
-        setEditing(true);
-        wait = typeWait;
-      }
-    }
-  } else if (state === TypingState.DELETING) {
-    // delete char
-    setText(text.substring(0, text.length - 1));
-
-    if (charIndex === 0) {
-      // Phrase deletion completed
-      setEditing(false);
-      state = TypingState.TYPING;
-      wait = doneDeletingWait;
-    } else {
-      // Keep deleting
-      setEditing(true);
-      charIndex--;
-      wait = deleteWait;
-    }
+  if (currChar === CHAR_LONG_PAUSE) {
+    setEditing(false);
+    wait = longWait;
+  } else if (currChar === CHAR_SHORT_PAUSE) {
+    setEditing(false);
+    wait = shortWait;
+  } else if (currChar === CHAR_NEW_LINE) {
+    setText(text + "<br>");
+    setEditing(true);
+    wait = shortWait;
+  } else {
+    setEditing(true);
+    setText(text + currChar);
+    wait = typeWait;
   }
 
+  if (charIndex === message.length - 1) {
+    repeat = false;
+  } else {
+    charIndex++;
+  }
   return wait;
 };
 
