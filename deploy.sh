a58de30fafb56d0b921a911a29c2e704eee94763
Cristian Aldea <aldea.cristian@outlook.com>
2025-02-26 12:57:34 -0800

    fix deploy script removing CNAME

--- deploy.sh
+++ deploy.sh
@@ -4,17 +4,16 @@ git checkout main
 git branch -D gh-pages
 git checkout -b gh-pages
 
+rm -rf docs
 mkdir docs
 echo "www.cristianaldea.com" > docs/CNAME
 git add docs/
 git commit -m "Create CNAME"
 
 npm run build
-rm -rf docs
-mkdir docs
 mv dist/* docs
 git add docs/
 git commit -m "Deploy website"
 
-git push -u origin gh-pages --force-with-lease
+git push -u origin gh-pages --force
 git checkout main
